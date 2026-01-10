e5905b469b3db1fa3cc4db2279fbd962c424b924
DESKTOP-48CV98E\tlsrl <tlsrlvos2@naver.com>
2026-01-10 15:59:48 +0900

    initial commit

---
+++ src/test/java/com/bh/emotion_app/EmotionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bh.emotion_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
