b1d5ed13b0e58c71134a2b2c30f7f232c934020f
seonbh <68881063+seonbh@users.noreply.github.com>
2026-01-25 22:17:03 +0900

    TEST

---
+++ src/main/java/com/emotionmap/test/TestController.java
@@ -0,0 +1,12 @@
+package com.emotionmap.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello 감정연결지도 !";
+    }
+}
